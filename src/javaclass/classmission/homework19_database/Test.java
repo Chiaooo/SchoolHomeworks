33337a09e5d5c1980f90eedbba093291e9ab852d
Chiao <779442425@qq.com>
2021-11-01 17:42:24 +0800

    添加了JAVA第21次作业

--- src/javaclass/classmission/homework19_database/Test.java
+++ src/javaclass/classmission/homework19_database/Test.java
@@ -4,6 +4,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -13,7 +16,7 @@ import java.util.Scanner;
 public class Test {
 
 
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException, ParseException {
 		Statement stmt = null;
 		ResultSet rs = null;
 		String sql = null;
@@ -43,9 +46,41 @@ public class Test {
 		stmt = database.getConn().createStatement();
 
 		rs = stmt.executeQuery(sql);
-		
+
 		while (rs.next()) {
-			System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+			System.out.println(rs.getInt(1) + " " + rs.getString(2) + " "
+					+ rs.getString(3) + " " + rs.getString(4) + " " + rs.getString(5));
 		}
+
+		sql = "insert into t_login(username,password,sex,birthday) value(?,?,?,?) ";
+
+		System.out.println("请输入你要添加的用户名，密码，性别以及生日(yyyy-MM-dd)：");
+		username = in.nextLine();
+		password = in.nextLine();
+		String sex = in.nextLine();
+
+		String birthday = in.nextLine();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = sdf.parse(birthday);
+
+		pstmt = database.getConn().prepareStatement(sql);
+		pstmt.setString(1, username);
+		pstmt.setString(2, password);
+		pstmt.setString(3, sex);
+		pstmt.setDate(4, new java.sql.Date(date.getTime()));
+
+		pstmt.execute();
+		
+		System.out.println("插入成功！，新的数据信息如下");
+		sql = "select * from t_login";
+		stmt = database.getConn().createStatement();
+
+		rs = stmt.executeQuery(sql);
+
+		while (rs.next()) {
+			System.out.println(rs.getInt(1) + " " + rs.getString(2) + " "
+					+ rs.getString(3) + " " + rs.getString(4) + " " + rs.getString(5));
+		}
+
 	}
 }
